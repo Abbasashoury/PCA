c08465171af44ce4270ad8ae94c36839c431f910
Abbas Ashoury <abbasashoury84@gmail.com>
2026-07-23 16:01:02 +0330

    feat(Abbas): complete "__init__" of "data loader"

--- Src/Data_loader.py
+++ Src/Data_loader.py
@@ -1,3 +1,13 @@
 class DataLoader:
+    """
+    step 1
+    data loading...
+    """
     def __init__(self):
+        self.X = None # main matrix
+        self.y = None # tag: number
+        self.m = None # num of sample
+        self.n = None # num of feature
+
+    def load(self):
         pass
