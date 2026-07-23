73f1669194cbd80a954d9ecf4884a8ed4def7b0c
Abbas Ashoury <abbasashoury84@gmail.com>
2026-07-23 16:16:56 +0330

    feat(Abbas): Add "get_info func" to the "data loader" and rename variable

--- Src/Data_loader.py
+++ Src/Data_loader.py
@@ -1,3 +1,4 @@
+import numpy as np
 from sklearn.datasets import load_digits
 
 class DataLoader:
@@ -7,13 +8,20 @@ class DataLoader:
     """
     def __init__(self):
         self.X = None # main matrix
-        self.y = None # tag: number
-        self.m = None # num of sample
-        self.n = None # num of feature
+        self.t = None # tag: number
+        self.s = None # num of sample
+        self.f = None # num of feature
 
     def load(self):
         digits = load_digits()
         self.X = digits.data # matrix: (1800,64)
-        self.y = digits.target # tag: (0,9)
-        self.m, self.n = self.X.shape
-        return self.X, self.y
+        self.t = digits.target # tag: (0,9)
+        self.s, self.f = self.X.shape
+        return self.X, self.t
+
+    def get_info(self):
+        print("m (samples):", self.s)
+        print("n (features):", self.f)
+        print("X Shape:", self.X.shape)
+        print("y Shape:", self.t.shape)
+        print("unique labels", np.unique(self.t))
