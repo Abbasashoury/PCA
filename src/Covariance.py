1d1af5abee8ed6b7d2e17a1185e5ee453b61c32d
Abbas Ashoury <abbasashoury84@gmail.com>
2026-07-24 11:38:36 +0330

    feat(Abbas): Add "check_symmetry func" to the "Covariance class"

--- src/Covariance.py
+++ src/Covariance.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class Covariance:
     def __init__(self):
         pass
@@ -6,3 +9,7 @@ class Covariance:
         m = B.shape[0]  # num of sample
         C = (1.0 / (m - 1)) * (B.T @ B)
         return C
+
+    def check_symmetry(self, C, telorance):
+        is_symmetric = np.allclose(C, C.T, atol=telorance)
+        print(f"Symmetric: {is_symmetric}")
