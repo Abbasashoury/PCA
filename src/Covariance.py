f85a32b4383ff229b0c7da267da6aea55fc9a1b2
Abbas Ashoury <abbasashoury84@gmail.com>
2026-07-24 11:24:29 +0330

    feat(Abbas): Add "computeCovariance func" to the "Covariance class"

--- src/Covariance.py
+++ src/Covariance.py
@@ -1,3 +1,8 @@
 class Covariance:
     def __init__(self):
         pass
+
+    def computeCovariance(self, B):
+        m = B.shape[0]  # num of sample
+        C = (1.0 / (m - 1)) * (B.T @ B)
+        return C
