fix(Abbas): add telorance value to the "Covariance & Center classes"

src/Center.py
@@ -11,7 +11,7 @@ class Center:
         B = X - self.mean_vector  # centeralization
         return B
 
-    def verify_centering(self, B, telorance):
+    def verify_centering(self, B, telorance=1e-8):
         col_means = np.mean(B, axis=0)
         max_mean = np.max(np.abs(col_means))
         is_centered = max_mean < telorance

src/Covariance.py
@@ -10,7 +10,7 @@ class Covariance:
         C = (1.0 / (m - 1)) * (B.T @ B)
         return C
 
-    def check_symmetry(self, C, telorance):
+    def check_symmetry(self, C, telorance=1e-8):
         is_symmetric = np.allclose(C, C.T, atol=telorance)
         print(f"Symmetric: {is_symmetric}")
 