516693fa547f3bc1d695f8b7c23d8256cc09bffa
Abbas Ashoury <abbasashoury84@gmail.com>
2026-07-24 11:07:50 +0330

    feat(Abbas): Add "step 2" to the "main"

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from src.Data_loader import DataLoader
+from src.Center import center
+
 
 def main():
     print("=== Step 1: Load data ===")
     loader = DataLoader()
     X, t = loader.load()
     loader.get_info()
+
+    print("\n=== Step 2: Center data ===")
+    Center = center()
+    B = Center.center(X)
+    Center.verify_centering(B)
+
 
 if __name__ == '__main__':
     main()
