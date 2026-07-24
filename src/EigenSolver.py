d5fe4e37ab6a3513494b505b2cd59981e52130dc
Abbas Ashoury <abbasashoury84@gmail.com>
2026-07-24 19:59:56 +0330

    feat(Abbas): Add "verify_orthogonality func" to the "EigenSolver class"

--- src/EigenSolver.py
+++ src/EigenSolver.py
@@ -13,3 +13,12 @@ class EigenSolver:
         self.eigenvalues = eigenvalues[order]
         self.eigenvectors = eigenvectors[:, order]
         return self.eigenvalues, self.eigenvectors
+
+    def verify_orthogonality(self, telorance=1e-8):
+        W = self.eigenvectors
+        product = W.T @ W
+        identity = np.eye(W.shape[1])
+        max_diff = np.max(np.abs(product - identity))
+        is_orthogonal = max_diff < telorance
+        print(f"max W^T W - I: {max_diff}")
+        print(f"Orthogonal: {is_orthogonal}")
