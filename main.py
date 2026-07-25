576fb66b1db0df5c3ce3018c116283d41ff10207
Abbas Ashoury <abbasashoury84@gmail.com>
2026-07-25 10:10:37 +0330

    feat(Abbas): Add "step 6" to the "main"

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.DataLoader import dataLoader
 from src.Center import Center
 from src.Covariance import Covariance
 from src.EigenSolver import EigenSolver
+from src.PCA import PCA
 
 
 def main():
@@ -25,6 +26,11 @@ def main():
     solver = EigenSolver()
     eigenvalues, eigenvectors = solver.solveEigen(C)
     solver.verify_orthogonality()
+
+    print("\n=== Step 6 :Explained variance == =")
+    pca = PCA(components=10)
+    rel_variance, cum_variance = pca.compute_variance(eigenvalues)
+    k90 = pca.find_k_component(target=0.90)
 
 
 if __name__ == '__main__':
