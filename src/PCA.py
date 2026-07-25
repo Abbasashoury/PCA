fd1e5440bbcb49bfd490bbcc3a34c8d4c5263342
Abbas Ashoury <abbasashoury84@gmail.com>
2026-07-25 09:51:02 +0330

    feat(Abbas): Add "compute_variance func" to the "PCA class"

--- src/PCA.py
+++ src/PCA.py
@@ -1,6 +1,15 @@
+import numpy as np
+
+
 class PCA:
     def __init__(self, components):
         self.k = components  # num of components
         self.W = None  # projection matrix
         self.relative_variance = None
         self.cumulative_variance = None
+
+    def compute_variance(self, eigenvalues):
+        total = np.sum(eigenvalues)
+        self.relative_variance = eigenvalues / total
+        self.cumulative_variance = np.cumsum(self.relative_variance)
+        return self.relative_variance, self.cumulative_variance
