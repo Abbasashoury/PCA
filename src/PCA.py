ff06e276c2ed84e1754f35275572479859abd37f
Abbas Ashoury <abbasashoury84@gmail.com>
2026-07-25 09:34:30 +0330

    feat(Abbas): Add "__init__" to the ""PCA class"

--- src/PCA.py
+++ src/PCA.py
@@ -1,3 +1,6 @@
 class PCA:
-    def __init__(self):
-        pass
+    def __init__(self, components):
+        self.k = components  # num of components
+        self.W = None  # projection matrix
+        self.relative_variance = None
+        self.cumulative_variance = None
