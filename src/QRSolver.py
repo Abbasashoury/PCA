b19c1bdbb69f77b1e4bf56fb4a1d17c2e7ebeea3
Amin Mohammad Jabbari <amin.jabbari2006@gmail.com>
2026-08-04 11:26:12 +0330

    feat(Amin): Add "qr_iteration func" to the "QRSolver".

--- src/QRSolver.py
+++ src/QRSolver.py
@@ -1,3 +1,9 @@
+import numpy as np
+
 class QRSolver:
-    def __init__(self):
-        pass
+    def qr_iteration(self, A, num_iterations=1):
+        A_k = A.copy()
+        for _ in range(num_iterations - 1):
+            R,Q = np.linalg.qr(A_k)
+            A_k = R @ Q
+        return A_k
