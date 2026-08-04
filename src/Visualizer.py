83ecc83865a412b59f4fee39fca4c0cfcae8798c
Amin Mohammad Jabbari <amin.jabbari2006@gmail.com>
2026-08-04 20:37:39 +0330

    feat(Amin): Add "show_sample func" to the "Visualizer ".

--- src/Visualizer.py
+++ src/Visualizer.py
@@ -1,3 +1,11 @@
+import matplotlib.pyplot as plt
+
 class Visualizer:
-    def __init__(self):
-        pass
+
+    def plot_sample_image(self, X, y, index=0):
+        image = X[index].reshape(8, 8)
+        plt.figure(figsize=(3, 3))
+        plt.imshow(image, cmap="gray")
+        plt.title(f"Label: {X[index]}")
+        plt.axis("off")
+        plt.show()
