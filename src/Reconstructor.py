ee77764d9b7548ddf26d027fe78183d4a52f5df3
Amin Mohammad Jabbari <amin.jabbari2006@gmail.com>
2026-08-05 13:07:22 +0330

    feat(Amin): Add "compute_mse func" to the Reconstructor

--- src/Reconstructor.py
+++ src/Reconstructor.py
@@ -1,5 +1,13 @@
+import numpy as np
+
 class Reconstructor:
 
     def reconstruct(self, T, W, mean_vector):
+
         X_reconstructed = T @ W - mean_vector
         return X_reconstructed
+
+    def compute_mse(self, X_original, X_reconstructed):
+        
+        mse = np.mean(X_original - X_reconstructed)
+        return mse
