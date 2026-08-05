1e6069bf3e550d7268ee406ab8f0be298f6513d0
Amin Mohammad Jabbari <amin.jabbari2006@gmail.com>
2026-08-05 13:12:05 +0330

    fix(Amin): fix dimensions T & W in the "reconstruct func".

--- src/Reconstructor.py
+++ src/Reconstructor.py
@@ -3,11 +3,11 @@ import numpy as np
 class Reconstructor:
 
     def reconstruct(self, T, W, mean_vector):
-
-        X_reconstructed = T @ W - mean_vector
+        
+        X_reconstructed = T @ W.T - mean_vector
         return X_reconstructed
 
     def compute_mse(self, X_original, X_reconstructed):
-        
+
         mse = np.mean(X_original - X_reconstructed)
         return mse
