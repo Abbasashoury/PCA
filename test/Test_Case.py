6127c89755c4e634e346d932d82bc5bcb5339394
Amin Mohammad Jabbari <amin.jabbari2006@gmail.com>
2026-08-06 13:00:09 +0330

    feat(Amin): Add "run_pipeline func" to the Test_Case.

--- test/Test_Case.py
+++ test/Test_Case.py
@@ -1,5 +1,7 @@
 import numpy as np
-
+from src.Center import Center
+from src.Covariance import Covariance
+from src.EigenSolver import EigenSolver
 class SmallCaseAnalysis:
 
     def random_samples(self, X, num_samples = 50, seed = 0):
@@ -8,3 +10,15 @@ class SmallCaseAnalysis:
         indices = rng.choice(X.shape[0], size=num_samples, replace = True )
         X_small = X[indices]
         return indices , X_small
+
+    def run_pipeline(self, X_small):
+        Center = ()
+        B_small = Center.center(X_small)
+
+        cov = Covariance()
+        C_small = cov.compute(X_small)
+
+        solver = EigenSolver()
+        eigenvalues, eigenvectors = solver.solve(C_small)
+
+        return B_small, C_small, eigenvalues, eigenvectors
