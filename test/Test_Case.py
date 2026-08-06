38bb9e1e23a25ada55ce56ee9820b5bb46788c80
Amin Mohammad Jabbari <amin.jabbari2006@gmail.com>
2026-08-06 22:55:06 +0330

    fix(Amin): fix unauthorized duplicate samples in "random_samples func" to the Test_Case.

--- test/Test_Case.py
+++ test/Test_Case.py
@@ -7,7 +7,7 @@ class SmallCaseAnalysis:
     def random_samples(self, X, num_samples = 50, seed = 0):
 
         rng = np.random.default_rng( seed )
-        indices = rng.choice(X.shape[0], size=num_samples, replace = True )
+        indices = rng.choice(X.shape[0], size=num_samples, replace = False )
         X_small = X[indices]
         return indices , X_small
 
