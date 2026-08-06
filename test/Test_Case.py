d8d12f1c0f7384f7a439b29f9fdc777636716d9f
Amin Mohammad Jabbari <amin.jabbari2006@gmail.com>
2026-08-06 12:27:02 +0330

    feat(Amin): Add "random_samples func" to the "Test_Case"

--- test/Test_Case.py
+++ test/Test_Case.py
@@ -1,3 +1,10 @@
+import numpy as np
+
 class SmallCaseAnalysis:
-    def __init__(self):
-        print("hello WORLD")
+
+    def random_samples(self, X, num_samples = 50, seed = 0):
+
+        rng = np.random.default_rng( seed )
+        indices = rng.choice(X.shape[0], size=num_samples, replace = True )
+        X_small = X[indices]
+        return indices , X_small
