5e1b32c111588435ea774ae736ed45c368bdf290
Amin Mohammad Jabbari <amin.jabbari2006@gmail.com>
2026-08-07 12:35:25 +0330

    fix(Amin): fix analyze_eigenvalues method name in "Step 10" to the "main".

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ def main():
     analysis = SmallCaseAnalysis()
     X_small, indices = analysis.random_samples(X, num_samples = 50)
     B_small, C_small, eig_small, vec_small = analysis.run_pipeline(X_small)
-    analysis.analyze_zero_eigenvalues(eig_small)
+    analysis.analyze_eigenvalues(eig_small)
     analysis.analyze_null_space(eig_small)
 
 if __name__ == '__main__':
